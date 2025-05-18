4892f324aeb5ef158dc41aadf34df3fd95d4f093
fum1ple <fumiya.n0702@gmail.com>
2025-05-18 16:51:11 +0900

    [add]private folders

---
+++ routing-demo/src/app/dashboard/page.tsx
@@ -0,0 +1,16 @@
+function BarChart() {
+  return (
+    <div>
+      <h2>Bar Chart</h2>
+      <p>This is a bar chart.</p>
+    </div>
+  );
+}
+
+export default function Dashboard() {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+    </div>
+  );
+}
