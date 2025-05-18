3515aca3414da7b16212c8c4669ea492c7f67d83
fum1ple <fumiya.n0702@gmail.com>
2025-05-18 17:14:15 +0900

    [add]layout

--- routing-demo/src/app/layout.tsx
+++ routing-demo/src/app/layout.tsx
@@ -10,7 +10,23 @@ export default function RootLayout({
 }) {
   return (
     <html>
-      <body>{children}</body>
+      <body>
+        <header
+          style={{
+            backgroundColor: "lightblue",
+            padding: "1rem",
+          }}>
+          <p>header</p>
+        </header>
+        {children}
+        <footer
+          style={{
+            backgroundColor: "ghostwhite",
+            padding: "1rem",
+          }}>
+          <p>Footer</p>
+        </footer>
+      </body>
     </html>
   );
 }
